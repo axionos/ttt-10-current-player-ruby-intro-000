b0c05dd0d944cf3ce9d0fd0bc7c68cb2ade0648f
SOYEONG OH <axionoso@gmail.com>
2019-03-12 16:17:52 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,22 @@
+# occupied 1 = X
+# occupied 2 = O
+
+def turn_count(board)
+  counter = 0
+  board.each do |idx|
+    # count = board.count("X") + board.count("O")
+    if board[idx] != " " || board[idx] != ""
+      counting = board.count
+    end
+    counter += 1
+  end
+  return counting
+end
+
+def current_player(board, turns)
+  if turns % 2 == 0
+    return "X"
+  else turns % 2 != 0
+    return "O"
+  end
+end
